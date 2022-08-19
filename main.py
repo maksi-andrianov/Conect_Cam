19e4e35a6857be13e699b92d260d316982708cde
max0480 <maksi-andrianov@yandex.ru>
2022-08-19 10:41:21 +0300

    Доделан вывод в выходной файл Сделал проверку на уже имеющиеся значения в файле, добавлен отдельный поток под опрос каждой камеры, добавлено имя камеры при создание json файла

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ from threading import Thread
 import time
 
 def load_config():
-    import configparser  # импортируем библиотеку
+    import configparser
     print('Загружаем конфигурацию...')
-    config = configparser.ConfigParser()  # создаём объекта парсера
+    config = configparser.ConfigParser()
     config.read('config.ini')
-    print(config['CAM1']['IP'])
+    #print(config['CAM1']['IP'])
+    #print(config['CAM1'])
     IP_CAM1 = config['CAM1']['IP']
-    print('ip_cam1',IP_CAM1)
+    N_CAM1 = config['CAM1']['N_CAM']
+    #print('ip_cam1',IP_CAM1)
     PORT = config['CAM1']['PORT']
     IP_CAM2 = config['CAM2']['IP']
-    return IP_CAM1, IP_CAM2, PORT
+    N_CAM2 = config['CAM2']['N_CAM']
+    return IP_CAM1,N_CAM1, IP_CAM2,N_CAM2, PORT
 
 
 
-def conect_cam(IP,PORT):
+def conect_cam(IP,PORT,N_CAM):
     import socket
     import json
     from datetime import datetime
-    print('ip,port', IP, PORT)
-    #HOST, PORT = IP, PORT
-    # m ='{"id": 2, "name": "abc"}'
+    print('ip,port,№CAM', IP, PORT, N_CAM)
     m = "23" "{\"ACK\":\"\",\"RET\":0}\n\u0003r"  # словарь из лог файла программы DATA MATRIX HONEYWELL
     data = json.dumps(m)
     # Create a socket (SOCK_STREAM means a TCP socket WINDOWS)
@@ -34,79 +35,61 @@ def conect_cam(IP,PORT):
         print('Камера подключена')
         sock.sendall(bytes(data, encoding="utf-8"))
         print('Сообщение на камеру отправлено')
-        # data_out = {'code_id':'','create_date':''}
         data_out = {}
         while True:
-            # Receive data from the server and shut down
-            received = sock.recv(1024)
-            print('Тип recived', type(received))
+            # Подключаемся к серверу, если соединения нет, перезапускаем подключение
+            received = sock.recv(256)
+            #print('Тип recived', type(received))
             received = received.decode("utf-8")
             received_data = received[:-7]
             print('received_data', received_data)
-            # received = received.split('\\')
             time_resiev = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
             print("Sent:     {}".format(data))
-            print("Received: {}".format(received))
-            print("Received: {}", received)
-            print(time_resiev)
-            # print('Тип recived',type(received))
-            filename = datetime.now().strftime('%Y-%m-%d')
-            print('Код', received[:-7])
+            #print("Received: {}".format(received))
+            #print("Received: {}", received)
+            #print(time_resiev)
+            filename = datetime.now().strftime('%Y-%m-%d_')
+            print('Считанный код', received[:-7])
+            N_CAM_chk = N_CAM
+            #print('N_CAM_chk++++++++++++++',str(filename)+N_CAM_chk,type(N_CAM_chk),type(filename))
             # chek_file()
-            if chek_file(received_data) == -1:
-                with open(str(filename) + '.json', 'a+') as out_file:
-                    # data_out = {}
+            if chek_file(received_data, N_CAM_chk) == -1:
+                with open(str(filename) + str(N_CAM)+'.json', 'a+') as out_file:
                     data_scan = {'code_id': received[:-7], 'create_date': time_resiev}
                     data_out.update({'code_id': received[:-7], 'create_date': time_resiev})
-                    # print('Тип дата аут', type(data_out))
-                    # for line in data_out:
-                    # print(type(line))
-                    # print('data_out=',data_out)
                     json.dump(data_out, out_file, sort_keys=False, separators=None, ensure_ascii=False)
-                    # out_file.write(str(data_out))
-                # json.dumps(format(data_out), out_file, indent=6)
-                # print(type(out_file))
-                # out_file.write('out_data')
-                # out_file.write(',')
-            # out_file.write(format(received) +'\n')
-            # for  line in json.dump(format(received)):
-            # file.write(line + '\n')
-            # received = sock.recv(1024)
-            # received = received.decode("utf-8")
-    # finaly:
     except:
         print('Ошибка связи или другая ошибка')
         sock.close()
-        print('  Подкючение закрыто, IP,PORT:(',IP,PORT,')')
-        IP_r, PORT_r = IP, PORT
-        reconect_cam(IP_r,PORT_r)
+        print('  Подкючение закрыто, IP,PORT,№CAM:(',IP,PORT,N_CAM,')')
+        IP_r, PORT_r,N_CAM_r = IP, PORT, N_CAM
+        reconect_cam(IP_r,PORT_r,N_CAM_r)
 
 
-def reconect_cam(IP_r, PORT_r):
+def reconect_cam(IP_r, PORT_r,N_CAM_r):
     import time
     connected = False
     time.sleep(10)
     while not connected:
-        print('Переподключение, IP,PORT:',IP_r,PORT_r)
+        print('Переподключение, IP,PORT, №Камеры:',IP_r,PORT_r,N_CAM_r)
         IP = IP_r
         PORT = PORT_r
-        conect_cam(IP,PORT)
+        N_CAM = N_CAM_r
+        conect_cam(IP,PORT, N_CAM)
 
 
 ##Проверяем файл JSON на наличие совпадающих значений ключа code_id
-def chek_file(received_data):
-    filename = datetime.now().strftime('%Y-%m-%d')
-    print('Проверяем файл')
+def chek_file(received_data,N_CAM):
+    from datetime import datetime
+    filename = datetime.now().strftime('%Y-%m-%d_')
+    print('Проверяем файл',filename+N_CAM,'.json')
     try:
-        with open(str(filename) + '.json', 'r') as fp:
-            # data = json.load(fp)
+        with open(str(filename) + str(N_CAM) +'.json', 'r') as fp:
             data = fp.read()
-            print('Data=', data)
-            print('Тип даты', type(data))
             index = data.find(received_data)
-            print('Индекс=', index)
     except:
         index = -1
+        print('Файла',filename+N_CAM,'.json не существует')
         print(index)
     finally:
         return index
@@ -128,18 +111,18 @@ def valid_ipv4_address(address):
 
 
 if __name__ == '__main__':
+    load_config()
     IP_CAM1 =load_config()[0]
-    IP_CAM2 = load_config()[1]
-    PORT = load_config()[2]
-    print(load_config()[2])
-    #IP_CAM1 = load_config[0]
-    #IP_CAM2 = IP_Cdef load_config[0]M2
-    t1 = Thread(target= conect_cam, args=(IP_CAM1, PORT))
-    t2 = Thread(target= conect_cam, args=(IP_CAM2, PORT))
+    N_CAM1 = load_config()[1]
+    IP_CAM2 = load_config()[2]
+    N_CAM2 = load_config()[3]
+    print('CAM2===',N_CAM2)
+    PORT = load_config()[4]
+    print(load_config()[4])
+    t1 = Thread(target= conect_cam, args=(IP_CAM1, PORT,N_CAM1))
+    t2 = Thread(target= conect_cam, args=(IP_CAM2, PORT,N_CAM2))
 
     t1.start()
     t2.start()
     t1.join()
     t2.join()
-    #conect_cam()
-    #reconect_cam()
